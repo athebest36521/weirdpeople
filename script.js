6c5cdf12d80c9351682d02a230726bf36266a099
athebest36521 <526atiwari@frhsd.com>
2024-11-05 11:54:42 -0500

    JS Commented

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-alert("The website is still not complete, yet...If you want to risk it, press OK");
+//added alerts to page
+alert("I'm really sorry, the website is still not complete, yet...However, if you still want to risk it, press OK");
+alert("I said IT DOESN'T WORK, I'M WARNING YOU");
 alert("ARE YOU SURE YOU WANT TO CONTINUE?!");
-alert("I said IT DOESN'T WORK, I'M WARNING YOU")
